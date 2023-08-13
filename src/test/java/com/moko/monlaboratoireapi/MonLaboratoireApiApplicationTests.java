62eb9392a8764be5af83cdf770babe2ef06889fc
Moko <moko.laboratoire@gmail.com>
2023-08-13 20:47:37 +0200

    Hello world

---
+++ src/test/java/com/moko/monlaboratoireapi/MonLaboratoireApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moko.monlaboratoireapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonLaboratoireApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
